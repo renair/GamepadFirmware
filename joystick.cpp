2a4684b932e6270626d2d622fb66cbf8593d067d
renair <platinium9889@gmail.com>
2018-06-01 00:53:18 +0300

    Fix bit constants

--- joystick.cpp
+++ joystick.cpp
@@ -13,20 +13,25 @@ Joystick::Joystick(byte v, byte h, byte b):
 
 byte Joystick::getDirection()
 {
-  byte dead_zone_size( 30 );
+  static const byte dead_zone_size = 70;
   const int x = analogRead(_horizontal);
   const int y = analogRead(_vertical);
-  byte direction(0);
+  byte direction = 0;
   //horizontal direction
-  if (x > 512 + dead_zone_size){
+  if (x > 512 + dead_zone_size)
+  {
     direction |= RIGHT;
-  } else if(x < 512 - dead_zone_size){
+  } 
+  else if(x < 512 - dead_zone_size)
+  {
     direction |= LEFT;
   }
   //vertical direction
-  if (y > 512 + dead_zone_size){
+  if (y > 512 + dead_zone_size)
+  {
     direction |= UP;
-  } else if(y < 512 - dead_zone_size){
+  } else if(y < 512 - dead_zone_size)
+  {
     direction |= DOWN;
   }
   return direction;
--- joystick.h
+++ joystick.h
@@ -4,14 +4,14 @@
 #include <Arduino.h>
 
 const byte CENTER = 0b0000;
-const byte RIGHT = 0b0001;
-const byte LEFT = 0b0010;
-const byte UP = 0b0011;
-const byte UP_RIGHT = 0b0100;
-const byte UP_LEFT = 0b0101;
-const byte DOWN = 0b0110;
-const byte DOWN_RIGHT = 0b0111;
-const byte DOWN_LEFT = 0b1000;
+const byte UP = 0b1000;
+const byte UP_RIGHT = 0b1100;
+const byte RIGHT = 0b0100;
+const byte RIGHT_DOWN = 0b0110;
+const byte DOWN = 0b0010;
+const byte DOWN_LEFT = 0b0011;
+const byte LEFT = 0b0001;
+const byte LEFT_UP = 0b1001;
 
 class Joystick {
   private:
